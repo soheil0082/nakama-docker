f1ed709ad0452270093b354f80f0250ab96e7337
soheil <40827869+soheil0082@users.noreply.github.com>
2025-04-30 16:47:08 +0330

    Initial Commit

---
+++ modules/time.lua
@@ -0,0 +1,79 @@
+local nk = require("nakama")
+
+local function reward_inviter(context, data)
+    local input = nk.json_decode(data)
+  
+    nk.storage_write({
+      {
+        collection = "users",
+        key = input.userId,
+        user_id = input.userId,
+        value = nk.json_decode(input.data),
+        permission_read = 2,
+      }
+    })
+end
+
+local function check_key(context, data)
+    local input = nk.json_decode(data)
+    local key = input.key
+    local collection = input.collection
+  
+    local result = nk.storage_read({
+      {
+        collection = collection,
+        key = key,
+        user_id = "0e19ff5e-3128-4796-a66e-9e443debb228"
+      }
+    })
+  
+    if #result == 0 then
+      return ""
+    end
+  
+    return result[1].value.Uid
+  end  
+
+  local function write_object(context, data)
+    local input = nk.json_decode(data)
+    local key = input.key
+    local collection = input.collection
+    local object = input.object
+    local id = input.id
+  
+    local ok, err = pcall(function()
+      if id ~= nil and #id > 1 then
+        nk.storage_write({
+          {
+            collection = collection,
+            value = nk.json_decode(object),
+            key = key,
+            user_id = id
+          }
+        })
+      else
+        nk.storage_write({
+          {
+            collection = collection,
+            value = nk.json_decode(object),
+            key = key,
+            user_id = "0e19ff5e-3128-4796-a66e-9e443debb228"
+          }
+        })
+      end
+        end)
+        
+        if not ok then
+            error("Could not write to the server. Error code: " .. tostring(err))
+        end
+  end  
+
+local function getservertime(context, payload)
+    return os.date("%Y%m%d%H%M%S")
+end
+
+nk.register_rpc(getservertime, "GetServerTime")
+nk.register_rpc(reward_inviter, "reward_inviter")
+nk.register_rpc(check_key, "check_key")
+nk.register_rpc(write_object, "write_object")
+
